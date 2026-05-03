a674fc91de43047065cf69a11d80705fbb0bfa24
Daniel Aboderin <aboderindaniel482@gmail.com>
2026-05-03 19:27:44 +0100

    refactor: simplify LandingPage by removing unused components and sections

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,68 +1,7 @@
-// Server Component — no "use client" directive
-// Interactive sections are extracted into focused client components
+import React from "react";
 
-import HeroSection from "@/components/shared/home/hero";
+const page = () => {
+  return <div>Landing page</div>;
+};
 
-import SectionErrorBoundary from "@/components/shared/home/SectionErrorBoundary";
-
-export default function LandingPage() {
-  return (
-    <div className="min-h-screen bg-white">
-      <SectionErrorBoundary name="HeroSection">
-        <HeroSection />
-      </SectionErrorBoundary>
-
-      {/* <SectionErrorBoundary name="TrustStrip">
-        <TrustStrip />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="CategoriesSection">
-        <CategoriesSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="FlashSaleSection">
-        <FlashSaleSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="DailyDealsSection">
-        <DailyDealsSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="FeaturedProductsSection">
-        <FeaturedProductsSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="LiveActivityTicker">
-        <LiveActivityTicker />
-      </SectionErrorBoundary> */}
-
-      {/* <SectionErrorBoundary name="PromoBannersSection">
-        <PromoBannersSection />
-      </SectionErrorBoundary> */}
-
-      {/* <SectionErrorBoundary name="NewArrivalsSection">
-        <NewArrivalsSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="ShopByBrandSection">
-        <ShopByBrandSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="TopVendorsSection">
-        <TopVendorsSection />
-      </SectionErrorBoundary> */}
-      {/* 
-      <SectionErrorBoundary name="HowItWorksSection">
-        <HowItWorksSection />
-      </SectionErrorBoundary>
-
-      <SectionErrorBoundary name="TestimonialsSection">
-        <TestimonialsSection />
-      </SectionErrorBoundary> */}
-
-      {/* <SectionErrorBoundary name="CtaSection">
-        <CtaSection />
-      </SectionErrorBoundary> */}
-    </div>
-  );
-}
+export default page;
