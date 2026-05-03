0001c5f6687e57cc9f7dd921b76036977e552289
Daniel Aboderin <aboderindaniel482@gmail.com>
2026-05-03 19:18:55 +0100

    refactor: comment out unused section imports and components in LandingPage

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -2,19 +2,7 @@
 // Interactive sections are extracted into focused client components
 
 import HeroSection from "@/components/shared/home/hero";
-import CategoriesSection from "@/components/shared/home/categories";
-import FlashSaleSection from "@/components/shared/home/FlashSaleSection";
-import FeaturedProductsSection from "@/components/shared/home/FeaturedProductsSection";
-import PromoBannersSection from "@/components/shared/home/PromoBannersSection";
-import DailyDealsSection from "@/components/shared/home/DailyDealsSection";
-import NewArrivalsSection from "@/components/shared/home/NewArrivalsSection";
-import ShopByBrandSection from "@/components/shared/home/ShopByBrandSection";
-import LiveActivityTicker from "@/components/shared/home/LiveActivityTicker";
-import TopVendorsSection from "@/components/shared/home/TopVendorsSection";
-import TrustStrip from "@/components/shared/home/TrustStrip";
-import HowItWorksSection from "@/components/shared/home/HowItWorksSection";
-import TestimonialsSection from "@/components/shared/home/TestimonialsSection";
-import CtaSection from "@/components/shared/home/CtaSection";
+
 import SectionErrorBoundary from "@/components/shared/home/SectionErrorBoundary";
 
 export default function LandingPage() {
@@ -24,7 +12,7 @@ export default function LandingPage() {
         <HeroSection />
       </SectionErrorBoundary>
 
-      <SectionErrorBoundary name="TrustStrip">
+      {/* <SectionErrorBoundary name="TrustStrip">
         <TrustStrip />
       </SectionErrorBoundary>
 
@@ -46,7 +34,7 @@ export default function LandingPage() {
 
       <SectionErrorBoundary name="LiveActivityTicker">
         <LiveActivityTicker />
-      </SectionErrorBoundary>
+      </SectionErrorBoundary> */}
 
       {/* <SectionErrorBoundary name="PromoBannersSection">
         <PromoBannersSection />
